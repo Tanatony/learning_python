60505edb746a01d53c7d4d52f86764b48a89dee9
Oleksandr Dikov <sander1687@gmail.com>
2019-05-25 21:50:53 +0300

    remove old exercises

--- pyneng/ex12.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-print(subprocess.run('ls'))
-print(subprocess.run('pwd'))
-print(subprocess.run('who'))
-print(subprocess.run(['ping', '-c', '3', '-n', '8.8.8.8']))
--- pyneng/ex7_1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-with open('r1.txt') as src, open('r2.txt', 'w') as dest:
-    for line in src:
-        if line.startswith('service'):
-            dest.write(line)
